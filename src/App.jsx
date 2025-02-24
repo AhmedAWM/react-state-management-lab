1358f633c6116dc7de5fa7edf0003bc2f079a673
AhmedAWM <a.ahmed.aw@gmail.com>
2025-02-24 14:45:09 +0300

    Lab

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import ZombieFighters from './data/ZombieFighters'; // Seperating the data from App.jsx is much cleaner
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [team, setTeam] = useState([]);
+  const [money, setMoney] = useState(100);
+  const [zombieFighters, setZombieFighters] = useState(ZombieFighters);
+
+  function handleAddFighter(fighter) {
+    if(money < fighter.price) {
+      console.log('No enough money to buy');
+      return;
+    }
+
+    // If enough money, add the fighter to the team
+    setTeam([...team, fighter]);
+    
+    // Remove the fighter from zombie fighters list
+    const updatedZombieFighters = zombieFighters.filter((zFighter) => {
+      return zFighter !== fighter.id; // Do nothing if IDs does not match
+    });
+    setZombieFighters(updatedZombieFighters); // Otherwise, remove and update zombieFighter's list
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div class="container">
+        <h2>{ zombieFighters[0].name }</h2>
+        <center><img src={ zombieFighters[0].img } alt={ zombieFighters[0].name } /></center>
+        <div>
+          <span>Price: ${ zombieFighters[0].price }</span>
+          <span>Strength: { zombieFighters[0].strength }</span>
+          <span>Agility: { zombieFighters[0].agility }</span>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
