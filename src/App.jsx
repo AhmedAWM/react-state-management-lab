b57af331caebcec52485f3bd5273a689d7fd672c
AhmedAWM <a.ahmed.aw@gmail.com>
2025-02-24 15:44:10 +0300

    Lab

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,83 @@ const App = () => {
   const [money, setMoney] = useState(100);
   const [zombieFighters, setZombieFighters] = useState(ZombieFighters);
 
-  function handleAddFighter(fighter) {
-    if(money < fighter.price) {
+  function handleAddFighter(addFighter) {
+    if(money < addFighter.price) {
       console.log('No enough money to buy');
       return;
     }
 
     // If enough money, add the fighter to the team
-    setTeam([...team, fighter]);
-    
+    setTeam([...team, addFighter]);
+
     // Remove the fighter from zombie fighters list
-    const updatedZombieFighters = zombieFighters.filter((zFighter) => {
-      return zFighter !== fighter.id; // Do nothing if IDs does not match
+    const updatedZombieFighters = zombieFighters.filter((oneFighter) => {
+      return oneFighter.id !== addFighter.id;
     });
-    setZombieFighters(updatedZombieFighters); // Otherwise, remove and update zombieFighter's list
+
+    // Otherwise, remove and update zombieFighter's list
+    setZombieFighters(updatedZombieFighters);
+
+    // Deduct money from money account
+    setMoney(money - addFighter.price);
   }
+
+  // Removeing fighters from team
+  function handleRemoveFromTeam(removedFighter) {
+    // Remove fighter from team
+    setTeam(
+      team.filter((oneFighter) => {
+        return oneFighter.id !== removedFighter.id;
+      })
+    )
+
+    // Add fighter back to the zombieFighters list
+    setZombieFighters([...zombieFighters, removedFighter]);
+
+    // Refund the money to the account
+    setMoney(money + removedFighter.price);
+  }
+
+  // Calculate total strength and agility
+  const totalStrength = team.reduce((total, fighter) => total + fighter.strength, 0);
+  const totalAgility = team.reduce((total, fighter) => total + fighter.agility, 0);
 
   return (
     <>
-      <div class="container">
-        <h2>{ zombieFighters[0].name }</h2>
-        <center><img src={ zombieFighters[0].img } alt={ zombieFighters[0].name } /></center>
-        <div>
-          <span>Price: ${ zombieFighters[0].price }</span>
-          <span>Strength: { zombieFighters[0].strength }</span>
-          <span>Agility: { zombieFighters[0].agility }</span>
-        </div>
+      <center><h1>Available Fighters</h1></center>
+      <div className='container'>
+        {
+          zombieFighters.map((fighter) => (
+            <section key={ fighter.id }>
+              <h2>{ fighter.name }</h2>
+              <center><img src={ fighter.img } alt={ fighter.name } /></center>
+              <div>
+                <span>Price: ${ fighter.price }</span>
+                <span>Strength: { fighter.strength }</span>
+                <span>Agility: { fighter.agility }</span>
+              </div>
+              <button onClick={ () => handleAddFighter(fighter) }>Add</button>
+            </section>
+          )) 
+        }
+      </div>
+
+      <center><h1>Team Fighters</h1></center>
+      <div className='container'>
+        {
+          team.map((fighter) => (
+            <section key={ fighter.id }>
+              <h2>{ fighter.name }</h2>
+              <center><img src={ fighter.img } alt={ fighter.name } /></center>
+              <div>
+                <span>Price: ${ fighter.price }</span>
+                <span>Strength: { fighter.strength }</span>
+                <span>Agility: { fighter.agility }</span>
+              </div>
+              <button onClick={ () => handleRemoveFromTeam(fighter) }>Delete</button>
+            </section>
+          )) 
+        }
       </div>
     </>
   );
